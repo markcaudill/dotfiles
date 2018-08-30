c91d8f8d5bd6b2c563b9aaca91a15da98684ab7f
Mark Caudill <markcaudill@gmail.com>
2018-08-30 14:28:32 -0400

    Clear greeting.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,6 @@
-set -x PATH ~/bin $PATH
-set -U EDITOR vim
+set fish_greeting # Clear greeting
+
+add_to_user_path ~/bin
+set --export EDITOR vim
+set --export GIT_EDITOR vim
+set --export PYTHONPATH $PYTHONPATH ~/src/ansible/lib
