2be619b0297aea4e5f77cf28c0b9e0d4364409ec
Mark Caudill <mark@mrkc.me>
2019-01-22 18:07:33 +0000

    Redirect ssh_agent output to STDERR.
    This was breaking rsync and scp.

--- .config/fish/functions/ssh_agent.fish
+++ .config/fish/functions/ssh_agent.fish
@@ -1,11 +1,11 @@
 # Source: https://superuser.com/a/966942/40900
 function ssh_agent --description 'launch the ssh-agent and add the id_rsa identity'
   if begin
-    set -q SSH_AGENT_PID
-    and kill -0 $SSH_AGENT_PID
-    and grep -q '^ssh-agent' /proc/$SSH_AGENT_PID/cmdline
-  end
-    echo "ssh-agent running on pid $SSH_AGENT_PID"
+      set -q SSH_AGENT_PID
+      and kill -0 $SSH_AGENT_PID
+      and grep -q '^ssh-agent' /proc/$SSH_AGENT_PID/cmdline
+    end
+    echo "ssh-agent running on pid $SSH_AGENT_PID" 1>&2
   else
     eval (command ssh-agent -c | sed 's/^setenv/set -Ux/')
   end
