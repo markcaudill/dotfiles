76f44ed151ee274e8c779ca896449d9f9842dd67
Mark Caudill <mark@mrkc.me>
2019-03-17 20:04:08 -0400

    Remove ssh_agent output.
    Having output like this seems to break certain automation tools that
    login over SSH and expect a shell prompt or something.

--- .config/fish/functions/ssh_agent.fish
+++ .config/fish/functions/ssh_agent.fish
@@ -6,7 +6,7 @@ function ssh_agent --description 'launch the ssh-agent and add the id_rsa identi
       and grep -q '^ssh-agent' /proc/$SSH_AGENT_PID/cmdline
     end
   else
-    eval (command ssh-agent -c | sed 's/^setenv/set -Ux/')
+    eval (command ssh-agent -c | sed 's/^setenv/set -Ux/') >/dev/null
   end
   set -l identity $HOME/.ssh/id_rsa
   set -l fingerprint (ssh-keygen -lf $identity | awk '{print $2}')
