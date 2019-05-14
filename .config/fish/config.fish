43688a6577a849c21a382021c625f3411942512d
Mark Caudill <mark@mrkc.me>
2019-05-14 09:38:10 -0400

    Set BROWSER and TREMINAL env vars.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,10 @@ set fish_greeting # Clear greeting
 add_to_user_path ~/bin
 add_to_user_path /usr/bin
 add_to_user_path /usr/sbin
+set --export BROWSER firefox
 set --export EDITOR vim
 set --export GIT_EDITOR vim
 set --export PYTHONPATH $PYTHONPATH ~/src/ansible/lib
+set --export TERMINAL gnome-terminal
 
 ssh_agent
