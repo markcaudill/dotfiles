2056356395644e98f22019f0cf70de4c22be4045
Mark Caudill <mark@mrkc.me>
2019-07-25 10:42:13 -0400

    Add defaults dir to role skeleton.

--- .config/fish/functions/ans-role.fish
+++ .config/fish/functions/ans-role.fish
@@ -1,17 +1,22 @@
 function ans-role --description "Create a new Ansible role skeleton"
     set role_name $argv[1]
     if test -d roles -a ! -x $role_name
-        mkdir -p roles/$role_name/{tasks,handlers,files,templates}
+        mkdir -p roles/$role_name/{tasks,handlers,defaults,files,templates}
         begin
             echo "---"
-            echo "# roles/$role_name/tasks/main.yaml"
+            echo "# $role_name/tasks/main.yaml"
             echo
         end > roles/$role_name/tasks/main.yaml
         begin
             echo "---"
-            echo "# roles/$role_name/handlers/main.yaml"
+            echo "# $role_name/handlers/main.yaml"
             echo
         end > roles/$role_name/handlers/main.yaml
+        begin
+            echo "---"
+            echo "# $role_name/defaults/main.yaml"
+            echo
+        end > roles/$role_name/defaults/main.yaml
     else
         echo "Can't file roles directory."
     end
