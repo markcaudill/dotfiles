b0e1e94eda867bc9ef4a23d14c533c9d9b36dc19
Mark Caudill <mark@mrkc.me>
2019-09-12 15:29:52 -0400

    Change http_get to hc_get.

--- lib/healthchecks.io.sh
+++ lib/healthchecks.io.sh
@@ -2,20 +2,20 @@ API_BASE="https://hc-ping.com"
 CURL_OPTS="-fsS --retry 3"
 
 hc_get() {
-    curl "${CURL_OPTS}" "${API_BASE}/${1}"
+    curl ${CURL_OPTS} "${API_BASE}/${1}" > /dev/null
 }
 
 hc_start() {
-    uuid="${1}"
-    http_get "${uuid}/start"
+    uuid="${1:-${HCUUID}}"
+    hc_get "${uuid}/start"
 }
 
 hc_fail() {
-    uuid="${1}"
-    http_get "${uuid}/fail"
+    uuid="${1:-${HCUUID}}"
+    hc_get "${uuid}/fail"
 }
 
 hc_success() {
-    uuid="${1}"
-    http_get "${uuid}"
+    uuid="${1:-${HCUUID}}"
+    hc_get "${uuid}"
 }
