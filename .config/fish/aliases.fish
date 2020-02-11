786e0e195bb39de89a88738bbd72947201207826
Mark Caudill <mark@mrkc.me>
2020-02-11 12:05:12 -0500

    Update Git abbreviations.

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -20,16 +20,38 @@ abbr --add ayear 'date -d "next year" "+%Y-%m-%d"'
 abbr --add ts date --iso-8601=seconds
 abbr --add tsf 'date --iso-8601=seconds | sed "s/://g"'
 abbr --add wget wget -c
-abbr --add gs  git status -sb
-abbr --add ga  git add
-abbr --add gc  git commit
-abbr --add gcm git commit -m
-abbr --add gca git commit --amend
-abbr --add gcl git clone
-abbr --add gco git checkout
-abbr --add gp  git push
-abbr --add gpl git pull
-abbr --add gl  git l
-abbr --add gd  git diff
-abbr --add gds git diff --staged
-abbr --add gf  git fetch
+abbr --add g    git
+abbr --add ga   git add
+abbr --add gaa  git add .
+abbr --add gaaa git add --all
+abbr --add gau  git add --update
+abbr --add gb   git branch
+abbr --add gbd  git branch --delete
+abbr --add gc   git commit
+abbr --add gcl  git clone
+abbr --add gcm  git commit --message
+abbr --add gcf  git commit --fixup
+abbr --add gco  git checkout
+abbr --add gcob git checkout -b
+abbr --add gcom git checkout master
+abbr --add gcos git checkout staging
+abbr --add gcod git checkout develop
+abbr --add gd   git diff
+abbr --add gda  git diff HEAD
+abbr --add gds  git diff --staged
+abbr --add glg  git log --graph --oneline --decorate --all
+abbr --add gld  git log --pretty=format:"%h %ad %s" --date=short --all
+abbr --add gm   git merge --no-ff
+abbr --add gma  git merge --abort
+abbr --add gmc  git merge --continue
+abbr --add gp   git pull
+abbr --add gpr  git pull --rebase
+abbr --add gr   git rebase
+abbr --add gs   git status --branch
+abbr --add gss  git status --branch --short
+abbr --add gst  git stash
+abbr --add gsta git stash apply
+abbr --add gstd git stash drop
+abbr --add gstl git stash list
+abbr --add gstp git stash pop
+abbr --add gsts git stash save
