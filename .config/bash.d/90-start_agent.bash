10b1674584032ead246d3e735d5fac167a7f4125
Mark Caudill <mark@mrkc.me>
2020-03-18 18:39:45 -0400

    Redirect to STDOUT to avoid breaking rsync and scp.

--- .config/bash.d/90-start_agent.bash
+++ .config/bash.d/90-start_agent.bash
@@ -1,9 +1,9 @@
 # shellcheck shell=bash
 start_agent() {
     local ssh_env="${1}"
-    echo "Initialising new SSH agent at ${ssh_env}..."
+    echo "Initialising new SSH agent at ${ssh_env}..." 1>&2
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${ssh_env}"
-    echo succeeded
+    echo succeeded 1>&2
     chmod 600 "${ssh_env}"
     # shellcheck source=/dev/null
     . "${ssh_env}" > /dev/null
