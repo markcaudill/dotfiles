31a2657ee2caf28bb42d41df6da8415e3ee8ddb4
Mark Caudill <mark@mrkc.me>
2020-11-10 12:57:01 -0500

    Fix return value for config init.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,11 @@ set fish_greeting # Clear greeting
 source ~/.config/fish/aliases.fish
 source ~/.config/fish/env-vars.fish
 
-#  Start SSH Agent if SSH_ENV exists and this is an interactive shell
-if status --is-interactive > /dev/null
+#  Start SSH Agent if this is an interactive shell
+if status is-interactive > /dev/null
     start_ssh_agent
 end
 
-command -v starship >/dev/null; and starship init fish | source
+if command -v starship >/dev/null
+    starship init fish | source
+end
