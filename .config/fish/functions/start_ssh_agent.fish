6fb465614bbf19bf85c9cbbfc3a2a4dde70f0446
Mark Caudill <mark@mrkc.me>
2020-11-10 12:56:39 -0500

    Fix SSH agent start.

--- .config/fish/functions/start_ssh_agent.fish
+++ .config/fish/functions/start_ssh_agent.fish
@@ -1,33 +1,36 @@
-function __start_ssh_agent
-    command ssh-agent -c | sed 's/^setenv/set -gx/' | sed 's/^echo/#echo/'
-end
-
 function __is_ssh_agent_running
     # Verify an SSH agent (defined by SSH_AGENT_PID) is running
-    kill -0 $SSH_AGENT_PID; or return 1
-
-    # Verify the SSH_AUTH_SOCK is present
-    test -S $SSH_AUTH_SOCK; or return 1
-
-    return 0
+    if kill -0 "$SSH_AGENT_PID" ^/dev/null >/dev/null
+        return 0
+    else
+        return 1
+    end
 end
 
 function __load_ssh_env
-    eval (cat $SSH_ENV) >/dev/null
+    # If the $SSH_ENV file is in C-shell format
+    if grep -E '^setenv' "$SSH_ENV" ^/dev/null >/dev/null
+        eval (cat "$SSH_ENV" | sed 's/^setenv/set -gx/' | sed 's/^echo/#echo/') >/dev/null
+    # If the $SSH_ENV is in the default format
+    else if grep 'export' "$SSH_ENV" ^/dev/null >/dev/null
+        eval (cat "$SSH_ENV" | sed 's/^\([^=]*\)=\([^;]*\).*/set -gx \1 \2; /g' | sed 's/^echo/#echo/g') >/dev/null
+    else
+    # Fall through: eval and hope for the best
+        eval (cat $SSH_ENV) >/dev/null
+    end
 end
 
 function start_ssh_agent
     # Try to load or start an agent
-    test -f $SSH_ENV; and __load_ssh_env
+    __load_ssh_env
     if not __is_ssh_agent_running
-        __start_ssh_agent > $SSH_ENV
+        command ssh-agent > "$SSH_ENV"
         __load_ssh_env
         if not __is_ssh_agent_running
-            echo "Unable to start SSH agent!"
-            env | grep SSH
             return 1
         end
     end
+
     # Load keys
     ssh-add -l >/dev/null; or ssh-add ^/dev/null
 end
