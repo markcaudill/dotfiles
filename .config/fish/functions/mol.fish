f5963e069be44a8ddddd540caef4c8c029cd47f4
Mark Caudill <mark@mrkc.me>
2021-03-03 10:14:30 -0500

    Add molecule function wrapping Docker.

---
+++ .config/fish/functions/mol.fish
@@ -0,0 +1,8 @@
+function mol
+	docker run --rm -it \
+		--log-driver none \
+		-v $PWD:$PWD \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		--name ansible_toolset \
+		quay.io/ansible/toolset:latest /bin/bash -c "cd $PWD && molecule $argv"
+end
