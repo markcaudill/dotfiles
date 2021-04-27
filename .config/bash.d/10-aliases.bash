5117286f8cbbd6c54e83befc654dc7c2937d6143
Mark Caudill <mark@mrkc.me>
2021-04-27 09:53:36 -0400

    Add completion to git bash aliases
    Not perfect since for things like gcob => git checkout -b, the
    completions doesn't know that the next item should be a branch name. But
    it's ok as long as that's kept in mind.

--- .config/bash.d/10-aliases.bash
+++ .config/bash.d/10-aliases.bash
@@ -75,43 +75,80 @@ alias wget='wget -c'
 
 # Git aliases
 alias g='git'
+__git_complete g git
 alias ga='git add'
+__git_complete ga git_add
 alias gaa='git add .'
+__git_complete gaa git_add
 alias gaaa='git add --all'
-alias gabr='abbr | grep git'
+__git_complete gaaa git_add
+alias gabr='alias | grep git'
 alias gau='git add --update'
+__git_complete gau git_add
 alias gb='git branch'
+__git_complete gb git_branch
 alias gbd='git branch --delete'
+__git_complete gbd git_branch
 alias gc='git commit'
+__git_complete gc git_commit
 alias gca='git commit --all --message'
+__git_complete gca git_commit
 alias gcl='git clone'
+__git_complete gcl git_clone
 alias gcm='git commit --message'
+__git_complete gcm git_commit
 alias gcf='git commit --fixup'
+__git_complete gcf git_commit
 alias gco='git checkout'
+__git_complete gco git_checkout
 alias gcob='git checkout -b'
-alias gcom='git checkout master'
+__git_complete gcob git_checkout
+alias gcom='git checkout main'
+__git_complete gcom git_checkout
 alias gcos='git checkout staging'
+__git_complete gcos git_checkout
 alias gcod='git checkout develop'
+__git_complete gcod git_checkout
 alias gd='git diff'
+__git_complete gd git_diff
 alias gda='git diff HEAD'
+__git_complete gda git_diff
 alias gds='git diff --staged'
+__git_complete gds git_diff
 alias gl='git log'
+__git_complete gl git_log
 alias glg='git log --graph --oneline --decorate --all'
+__git_complete glg git_log
 alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
+__git_complete gld git_log
 alias gm='git merge --no-ff'
+__git_complete gm git_merge
 alias gma='git merge --abort'
+__git_complete gma git_merge
 alias gmc='git merge --continue'
+__git_complete gmc git_merge
 alias gp='git pull'
+__git_complete gp git_pull
 alias gpr='git pull --rebase'
+__git_complete gpr git_pull
 alias gr='git rebase'
+__git_complete gr git_rebase
 alias gs='git status --branch'
+__git_complete gs git_status
 alias gss='git status --branch --short'
+__git_complete gss git_status
 alias gst='git stash'
+__git_complete gst git_stash
 alias gsta='git stash apply'
+__git_complete gsta git_stash
 alias gstd='git stash drop'
+__git_complete gstd git_stash
 alias gstl='git stash list'
+__git_complete gstl git_stash
 alias gstp='git stash pop'
+__git_complete gstp git_stash
 alias gsts='git stash save'
+__git_complete gsts git_stash
 
 alias path='echo $PATH | sed "s/:/\n/g"'
 
