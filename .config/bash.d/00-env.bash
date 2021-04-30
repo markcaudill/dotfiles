8570c788bc347ccc5a130e273e926079b82c77f4
Mark Caudill <mark@mrkc.me>
2021-04-30 11:17:12 -0400

    Satisfy shellcheck

--- .config/bash.d/00-env.bash
+++ .config/bash.d/00-env.bash
@@ -11,7 +11,7 @@ export GOBIN="${GOPATH}/bin"
 PATHS=("${HOME}/.local/bin" "${GOBIN}" "${HOME}/.local/go/bin" "/usr/local/go/bin" "/cygdrive/c/Go/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin")
 VALID_PATHS=
 # Only add a path to PATH if it it exists and isn't already in PATH. VALID_PATHS is used as intermediary variable
-for p in ${PATHS[@]}; do
+for p in "${PATHS[@]}"; do
 	if [[ -d "${p}" ]]; then
 		if [[ ":${VALID_PATHS}:" != *":${p}:"* ]]; then
 			VALID_PATHS="${VALID_PATHS:+"${VALID_PATHS}:"}${p}"
