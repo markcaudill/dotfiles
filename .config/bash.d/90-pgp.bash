08722d4b51a9d3c93a03316c04584c897711930e
Mark Caudill <mark@mrkc.me>
2022-01-26 11:42:30 -0500

    update with keys

--- .config/bash.d/90-pgp.bash
+++ .config/bash.d/90-pgp.bash
@@ -1,9 +1,9 @@
 secret () {
 	output=~/"${1}".$(date +%s).enc
-	gpg --encrypt --armor --output ${output} -r 0x0000 -r 0x0001 -r 0x0002 "${1}" && echo "${1} -> ${output}"
+	gpg --encrypt --armor --output "${output}" -r 250113B256B542E9 -r mark@cdll.me "${1}" && echo "${1} -> ${output}"
 }
 
 reveal () {
 	output=$(echo "${1}" | rev | cut -c16- | rev)
-	gpg --decrypt --output ${output} "${1}" && echo "${1} -> ${output}"
+	gpg --decrypt --output "${output}" "${1}" && echo "${1} -> ${output}"
 }
