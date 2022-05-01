52d85c2c5f07bf88a0909e8310a833bd0456c376
Mark Caudill <mark@mrkc.me>
2022-05-01 18:28:04 -0400

    formatting

--- .config/bash.d/00-env.bash
+++ .config/bash.d/00-env.bash
@@ -1,14 +1,18 @@
 # shellcheck shell=bash
 
 export CDPATH=".:~"
-export HISTCONTROL=ignoredups
-export HISTSIZE=2147483648
-export HISTFILESIZE="${HISTSIZE}"
-export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
-export HISTIGNORE=" *:ls:cd:cd -:pwd:exit:date"
-export GOPATH="${XDG_DATA_HOME:-$HOME/.local/share}/go"
+export EDITOR=vim
 export GOBIN="${GOPATH}/bin"
+export GOPATH="${XDG_DATA_HOME:-$HOME/.local/share}/go"
+export GPG_TTY=$(tty)
+export HISTCONTROL=ignoredups
+export HISTFILESIZE="${HISTSIZE}"
+export HISTIGNORE=" *:ls:cd:cd -:pwd:exit:date"
+export HISTSIZE=2147483648
+export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
 export NVM_DIR="${HOME}/.nvm"
+export SSH_ENV=$HOME/.ssh/environment
+
 PATHS=("${HOME}/.local/bin" "${HOME}/bin" "${GOBIN}" "${GOBIN}" "${HOME}/.pulumi/bin/" "/snap/bin" "/usr/local/go/bin" "/cygdrive/c/Go/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin" "/usr/games")
 VALID_PATHS=
 # Only add a path to PATH if it it exists and isn't already in PATH. VALID_PATHS is used as intermediary variable
@@ -20,7 +24,3 @@ for p in "${PATHS[@]}"; do
 	fi
 done
 export PATH=${VALID_PATHS}
-export EDITOR=vim
-export SSH_ENV=$HOME/.ssh/environment
-GPG_TTY=$(tty)
-export GPG_TTY
