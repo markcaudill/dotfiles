3317c9b40e0afeb08880f53b901d4214e4f77072
Mark Caudill <mark@mrkc.me>
2022-05-01 20:46:38 -0400

    remove more unused aliases

--- .config/bash.d/10-aliases-and-functions.bash
+++ .config/bash.d/10-aliases-and-functions.bash
@@ -154,8 +154,3 @@ alias path='echo $PATH | sed "s/:/\n/g"'
 # Use Podman instead of Docker if it's installed
 command -v podman >/dev/null && alias docker=podman
 
-# todo.txt
-alias t='todo.sh'
-
-alias dot='dot -Ncolor="#E2E2E1" -Ecolor="#e1e1e1" -Nfontcolor="#e1e1e1" -Efontcolor="#e1e1e1" -Gbgcolor="#282828" -Efontname="Fira Code" -Nfontname="Fira Code" -Gfontname="Fira Code" -Efontsize=12 -Nfontsize=12 -Gfontsize=12'
-alias icat='kitty +kitten icat --align=left --silent'
