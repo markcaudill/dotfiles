87850ebac4f96024bbb5f65286f0fa080b459fdc
Mark Caudill <mark@mrkc.me>
2022-07-05 14:58:51 -0400

    feat: add git stuff to prompt

--- .config/bash.d/10-prompt.bash
+++ .config/bash.d/10-prompt.bash
@@ -1,7 +1,20 @@
 # shellcheck shell=bash
 
-PS1="\n# \u@\h:\w\n; "
-export PS1
+# source ~/.git-prompt.sh if it exists
+if [ -f ~/.git-prompt.sh ]; then
+	export GIT_PS1_SHOWDIRTYSTATE=true
+	export GIT_PS1_SHOWSTASHSTATE=true
+	export GIT_PS1_SHOWUNTRACKEDFILES=true
+	export GIT_PS1_SHOWUPSTREAM="auto"
+	export GIT_PS1_HIDE_IF_PWD_IGNORED=true
+	export GIT_PS1_SHOWCOLORHINTS=true
+	# shellcheck source=/dev/null
+	. ~/.git-prompt.sh
+	export PROMPT_COMMAND='__git_ps1 "\n\W" "\\\$ "'
+else
+	export PS1='\n\W\$ '
+fi
+
 
 PS2="; "
 export PS2
