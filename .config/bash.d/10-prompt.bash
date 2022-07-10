c4c8e90dc3dff67d46e54159b39d81cec07dc7c9
Mark Caudill <mark@mrkc.me>
2022-07-10 12:36:15 -0400

    feat: change prompt to starship

--- .config/bash.d/10-prompt.bash
+++ .config/bash.d/10-prompt.bash
@@ -1,20 +1,7 @@
 # shellcheck shell=bash
 
-# source ~/.git-prompt.sh if it exists
-if [ -f ~/.git-prompt.sh ]; then
-	export GIT_PS1_SHOWDIRTYSTATE=true
-	export GIT_PS1_SHOWSTASHSTATE=true
-	export GIT_PS1_SHOWUNTRACKEDFILES=true
-	export GIT_PS1_SHOWUPSTREAM="auto"
-	export GIT_PS1_HIDE_IF_PWD_IGNORED=true
-	export GIT_PS1_SHOWCOLORHINTS=true
-	# shellcheck source=/dev/null
-	. ~/.git-prompt.sh
-	export PROMPT_COMMAND='__git_ps1 "\n\W" "\\\$ "'
-else
-	export PS1='\n\W\$ '
-fi
-
+PS1='\n\W\$ '
+export PS1
 
 PS2="; "
 export PS2
