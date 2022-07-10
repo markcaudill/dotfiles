dd3bab10637547a925ee899613b18d9411878bfb
Mark Caudill <mark@mrkc.me>
2022-07-10 12:46:09 -0400

    feat: add flyctl paths

--- .config/bash.d/00-env.bash
+++ .config/bash.d/00-env.bash
@@ -2,6 +2,7 @@
 
 export CDPATH=".:~"
 export EDITOR=vim
+export FLYCTL_INSTALL="${HOME}/.fly"
 export GOBIN="${GOPATH}/bin"
 export GOPATH="${XDG_DATA_HOME:-$HOME/.local/share}/go"
 export GPG_TTY=$(tty)
@@ -13,7 +14,7 @@ export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
 export NVM_DIR="${HOME}/.nvm"
 export SSH_ENV=$HOME/.ssh/environment
 
-PATHS=("${HOME}/.local/bin" "${HOME}/bin" "${GOBIN}" "${GOBIN}" "${HOME}/.pulumi/bin/" "/snap/bin" "/usr/local/go/bin" "/cygdrive/c/Go/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin" "/usr/games")
+PATHS=("${HOME}/.local/bin" "${HOME}/bin" "${FLYCTL_INSTALL}/bin" "${GOBIN}" "${GOBIN}" "${HOME}/.pulumi/bin/" "/snap/bin" "/usr/local/go/bin" "/cygdrive/c/Go/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin" "/usr/games")
 VALID_PATHS=
 # Only add a path to PATH if it it exists and isn't already in PATH. VALID_PATHS is used as intermediary variable
 for p in "${PATHS[@]}"; do
