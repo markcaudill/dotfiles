309158a51d8dda0f6a1068f5b3fe8e76e52a1ab2
Mark Caudill <mark@mrkc.me>
2022-07-14 18:08:20 -0500

    feat: move secret() and reveal()

--- .config/bash.d/20-aliases-and-functions.bash
+++ .config/bash.d/20-aliases-and-functions.bash
@@ -166,3 +166,13 @@ start_agent() {
     . "${ssh_env}" 1>&2 > /dev/null
     /usr/bin/ssh-add;
  }
+
+secret () {
+	output=~/"${1}".$(date +%s).enc
+	gpg --encrypt --armor --output "${output}" -r 250113B256B542E9 -r mark@cdll.me "${1}" && echo "${1} -> ${output}"
+}
+
+reveal () {
+	output=$(echo "${1}" | rev | cut -c16- | rev)
+	gpg --decrypt --output "${output}" "${1}" && echo "${1} -> ${output}"
+}
--- .config/bash.d/90-pgp.bash
+++
@@ -1,9 +0,0 @@
-secret () {
-	output=~/"${1}".$(date +%s).enc
-	gpg --encrypt --armor --output "${output}" -r 250113B256B542E9 -r mark@cdll.me "${1}" && echo "${1} -> ${output}"
-}
-
-reveal () {
-	output=$(echo "${1}" | rev | cut -c16- | rev)
-	gpg --decrypt --output "${output}" "${1}" && echo "${1} -> ${output}"
-}
