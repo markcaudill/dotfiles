e885ec56e5cf8f294a2d73f6a72e17f128141c74
Mark Caudill <mark@mrkc.me>
2022-08-18 17:05:34 +0100

    feat: add windows git path

--- .config/bash.d/00-env.bash
+++ .config/bash.d/00-env.bash
@@ -15,7 +15,7 @@ export HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S "
 export NVM_DIR="${HOME}/.nvm"
 export SSH_ENV=$HOME/.ssh/environment
 
-PATHS=("${HOME}/.local/bin" "${HOME}/bin" "${FLYCTL_INSTALL}/bin" "${GOBIN}" "${GOBIN}" "${HOME}/.pulumi/bin/" "/snap/bin" "/usr/local/go/bin" "/cygdrive/c/Go/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin" "/usr/games")
+PATHS=("${HOME}/.local/bin" "${HOME}/bin" "${FLYCTL_INSTALL}/bin" "${GOBIN}" "${GOBIN}" "${HOME}/.pulumi/bin/" "/snap/bin" "/usr/local/go/bin" "/cygdrive/c/Go/bin" "/usr/local/bin" "/usr/local/sbin" "/usr/bin" "/usr/sbin" "/bin" "/sbin" "/usr/games" "/cygdrive/c/Program Files/Git/cmd")
 VALID_PATHS=
 # Only add a path to PATH if it it exists and isn't already in PATH. VALID_PATHS is used as intermediary variable
 for p in "${PATHS[@]}"; do
